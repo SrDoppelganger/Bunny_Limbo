a5e00bfaa870f214b75a0318267aaedfd00cce1a
Sr.Doppelgänger <Arthur.albuquerque2004@gmail.com>
2024-05-11 17:28:07 -0300

    added basic title screen

---
+++ Scripts/title.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _input(event):
+	
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+		
+	elif event is InputEventKey and event.pressed:
+		get_tree().change_scene_to_file("res://Scenes/game.tscn")
